f7697a5807dec31ac53dcf9c7394035835b5eceb
tsykmn <109179668+tsykmn@users.noreply.github.com>
2024-05-01 11:33:39 -0700

    Time: 17 ms (21.76%), Space: 11.5 MB (72.21%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -11,17 +11,17 @@ class Solution(object):
             return 0
         elif n == 1 or n == 2:
             return 1
-            
+
         x, y, z = 0, 1, 1
         for i in range(2, n):
             x, y, z = y, z, x + y + z
         return z
 
         # Alternate solution: initialize dp/memoization
-        if n == 0:
-            return 0
-        elif n == 1 or n == 2:
-            return 1
+        # if n == 0:
+        #     return 0
+        # elif n == 1 or n == 2:
+        #     return 1
 
         # Tn+3 = Tn + Tn+1 + Tn+2
         # Tn = Tn+3 - (Tn+1 + Tn+2) --> Tn = Tn-3 + Tn-1 - Tn-2
