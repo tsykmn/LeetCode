cf729c1398e73d7de2e91498a471f6e68fbaf8b8
tsykmn <109179668+tsykmn@users.noreply.github.com>
2024-05-01 11:28:26 -0700

    Time: 11 ms (76.33%), Space: 11.5 MB (72.21%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    dp = {}
+    dp[1] = 1
+    dp[2] = 1
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+
+        if n in self.dp:
+            return self.dp[n]
+        self.dp[n] = self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
+        return self.dp[n]
+        
+        # x, y, z = 0, 1, 1
+        # for i in range(n):
+        #     z, y, x = x + y, x, y + z
+        # return x
