3b3d5d6d3691e2aec2d4c8bf32ba1c38c167a65e
tsykmn <109179668+tsykmn@users.noreply.github.com>
2024-05-01 11:33:30 -0700

    Time: 18 ms (16.82%), Space: 11.6 MB (37.9%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,12 +1,23 @@
 class Solution(object):
-    dp = {}
-    dp[1] = 1
-    dp[2] = 1
+    # dp = {}
+    # dp[1] = 1
+    # dp[2] = 1
     def tribonacci(self, n):
         """
         :type n: int
         :rtype: int
         """
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+            
+        x, y, z = 0, 1, 1
+        for i in range(2, n):
+            x, y, z = y, z, x + y + z
+        return z
+
+        # Alternate solution: initialize dp/memoization
         if n == 0:
             return 0
         elif n == 1 or n == 2:
@@ -18,8 +29,3 @@ class Solution(object):
             return self.dp[n]
         self.dp[n] = self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
         return self.dp[n]
-        
-        # x, y, z = 0, 1, 1
-        # for i in range(n):
-        #     z, y, x = x + y, x, y + z
-        # return x
