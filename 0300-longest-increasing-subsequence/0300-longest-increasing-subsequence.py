b3fa5fbceb7e90fab74a52ce1796b5c1b892f04a
tsykmn <109179668+tsykmn@users.noreply.github.com>
2024-05-01 10:45:39 -0700

    Time: 2058 ms (51.97%), Space: 12 MB (36.49%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -4,19 +4,35 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
+        # Initialize dp with a matrix
         n = len(nums)
         dp = [1]*n
         size = 0
 
-        for x in nums:
-            i, j = 0, size
-            while i != j:
-                m = (i+j)/2
-                if dp[m] < x:
-                    i = m+1
-                else:
-                    j=m
-            dp[i] = x
-            if i == size:
-                size += 1
-        return size
+        # Iterate from the last element of the sequence
+        for i in range(n - 1, -1, -1):
+            # Iterate elements after i
+            # check if the elements after i is larger than i or not
+            for j in range(i+1, n):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], dp[j]+1)
+        return max(dp)
+
+        # Alternate Solution
+        # n = len(nums)
+        # dp = [1]*n
+        # size = 0
+
+        # for x in nums:
+        #     i, j = 0, size
+        #     while i != j:
+        #         m = (i+j)/2
+        #         if dp[m] < x:
+        #             i = m+1
+        #         else:
+        #             j=m
+        #     dp[i] = x
+        #     if i == size:
+        #         size += 1
+        # return size
