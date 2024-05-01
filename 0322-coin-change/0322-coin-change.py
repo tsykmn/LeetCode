7b028063628a5435d64c7577e2ba00677cf20f4d
tsykmn <109179668+tsykmn@users.noreply.github.com>
2024-05-01 15:10:49 -0700

    Time: 742 ms (67.65%), Space: 12.3 MB (52.97%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+
+        n = len(coins)
+        dp = [amount + 1]*(amount + 1)
+        dp[0] = 0
+
+        if amount == 0:
+            return 0
+
+        for i in range(1, amount + 1):
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i], dp[i-c] + 1)
+        return dp[amount] if dp[amount] != amount + 1 else -1
