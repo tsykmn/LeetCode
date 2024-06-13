6fc4f9b20c4f897e84fbb5bca4dc570058b49823
tsykmn <109179668+tsykmn@users.noreply.github.com>
2024-06-12 17:16:37 -0700

    Time: 17 ms (47.18%), Space: 11.7 MB (56.39%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -4,18 +4,34 @@ class Solution(object):
         :type stones: List[int]
         :rtype: int
         """
-        def helper(stone):
-            if 1 == len(stone):
-                return stone[0]
-            elif len(stone) == 0:
-                return 0
-            n = len(stone)
-            n1 = max(stone)
-            stone.remove(n1)
-            n2 = max(stone)
-            stone.remove(n2)
-            if n1 != n2:
-                stone.append(abs(n1-n2))
-            return helper(stone)
-        return helper(stones)
+        stones.sort()
+        while stones:
+            s1 = stones.pop()
+            if not stones:
+                return s1
+            s2 = stones.pop()
+            if s1 > s2:
+                n = len(stones)
+                for i in range(n+1):
+                    if i == n or stones[i] >= s1-s2:
+                        stones.insert(i, s1-s2)
+                        break
+        return 0
+        # my solution
+        # make a helper function
+        # def helper(stone):
+        # if there is only 1 stone or none remaining
+        #     if 1 == len(stone):
+        #         return stone[0]
+        #     elif len(stone) == 0:
+        #         return 0
+        #     n = len(stone)
+        #     n1 = max(stone) # get the 1st biggest number
+        #     stone.remove(n1)
+        #     n2 = max(stone) # get the 2nd biggest number
+        #     stone.remove(n2)
+        #     if n1 != n2: # add if not same
+        #         stone.append(abs(n1-n2))
+        #     return helper(stone)
+        # return helper(stones)
         
