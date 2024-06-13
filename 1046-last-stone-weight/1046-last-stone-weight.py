68a069d4077aaaeb79972a2beb692745588b1452
tsykmn <109179668+tsykmn@users.noreply.github.com>
2024-06-12 17:04:43 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        if stones == [1]:
+            return 1
+        def helper(stone):
+            if 1 == len(stone):
+                return stone[0]
+            n = len(stone)
+            n1 = max(stone)
+            stone.remove(n1)
+            n2 = max(stone)
+            stone.remove(n2)
+            if n1 != n2:
+                stone.append(abs(n1-n2))
+            return helper(stone)
+        return helper(stones)
+        
