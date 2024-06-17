b333e42277d5184232d6eb6c99e271ea61e8f75a
tsykmn <109179668+tsykmn@users.noreply.github.com>
2024-06-16 23:56:33 -0700

    Time: 680 ms (77.2%), Space: 65.2 MB (77.51%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -9,8 +9,10 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
+        # 2 pointers: slow (keep track of middle) and fast (goes to end node)
         slow, fast = head, head
 
+        # move the pointers for slow and fast to reverse the half part of node
         while fast and fast.next:
             slow = slow.next
             if not fast.next.next:
@@ -18,12 +20,15 @@ class Solution(object):
             else:
                 fast = fast.next.next
 
+        # reverse the nodes for the endpart
         curr, nxt, prev = slow, None, None
         while curr:
             nxt = curr.next
             curr.next = prev
             prev = curr
             curr = nxt
+
+        # compare with the original list to see if valid palindrome
         rev = fast
         while head and rev:
             if head.val != rev.val:
