24674e3fb34514300cd8758779de5b38fb11606c
Teresa <109179668+tsykmn@users.noreply.github.com>
2024-11-18 23:39:40 -0800

    Time: 0 ms (100%), Space: 12.2 MB (6.1%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        dist = {')': '(', '}': '{', ']': '['}
+        opens = []
+        n = len(s)
+        for i in range(n):
+            if s[i] in dist.values():
+                opens.append(s[i])
+            if s[i] in dist:
+                if not opens:
+                    return False
+                p = opens.pop()
+                if dist[s[i]] != p:
+                    return False
+        return len(opens) == 0
