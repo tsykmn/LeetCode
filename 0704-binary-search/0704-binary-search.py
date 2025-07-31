693e559f14ed0752d53c6589d9323f4cf957e274
Teresa <109179668+tsykmn@users.noreply.github.com>
2025-07-30 23:13:10 -0700

    Time: 1 ms (13.64%), Space: 13.3 MB (64.28%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -5,27 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        # alternate
-        # initialize right (end) and left (start)
-        # if mid == target: index of mid
-        # if mid < target: left = mid + 1
-        # mid > target: mid - 1
         n = len(nums)
-        left = 0
-        right = n - 1
-        while left <= right:
-            middle = left + (right-left)/2
-            if nums[middle] == target:
-                return middle
-            elif nums[middle] < target:
-                left = middle + 1
-            elif nums[middle] > target:
-                right = middle - 1
+        for i in range(n):
+            if nums[i] == target:
+                return i
         return -1
-        # my way
-        # if target in nums:
-        #     n = len(nums)
-        #     for i in range(n):
-        #         if target == nums[i]:
-        #             return i
-        # return -1
