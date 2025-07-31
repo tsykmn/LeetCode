3a7126f11633062147724c159cce50e4a8f4c87d
Teresa <109179668+tsykmn@users.noreply.github.com>
2025-07-30 22:50:19 -0700

    Time: 22 ms (59.51%), Space: 12.5 MB (96.93%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        sDict, tDict = Counter(s), Counter(t)
+        return sDict == tDict
