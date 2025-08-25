8f4c5fede8a02bf36aab254ef5d1ca2a7094784c
Teresa <109179668+tsykmn@users.noreply.github.com>
2025-08-24 23:43:52 -0700

    Time: 54 ms (20.15%), Space: 13.2 MB (38.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,18 +4,29 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # make a set seen that adds characters that were already read
+        # if char in seen, duplicated char + needs to be removed from substring
+        # remove the previous characters from substring
+        # otherwise, add it to seen + adjust substring
+
+        # pointers
+        # left pointers at start and moves to right when duplicated char is seen
+        # right continues moving regardless
+        # left is used to adjust the substring when it was duplicated
+
         seen = set()
-        longest = 0
         n = len(s)
         left = 0
 
+        result = 0
+
         for right in range(n):
-            while s[right] in seen:
-                seen.remove(s[left])
-                left += 1
+            if s[right] in seen:
+                while s[right] in seen:
+                    seen.remove(s[left])
+                    left += 1
             seen.add(s[right])
-            longest = max(longest, len(seen))
-        return longest
 
-
+            result = max(len(seen), result)
         
+        return result
